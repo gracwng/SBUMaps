165c6c3c8f80217aef8bb2602927e599183b0829
Grace Wang <70417984+gracwng@users.noreply.github.com>
2023-11-19 10:30:49 -0500

    Update App.tsx
    brought back functional component (less verbose than class component): helpful video for these concepts https://www.youtube.com/watch?v=yc6elaGOoGQ

--- App.tsx
+++ App.tsx
@@ -1,13 +1,10 @@
-import { Component } from 'react';
 import {StyleSheet} from 'react-native'
 import { IndoorNav } from './screens/indoorMap/IndoorNav';
 
-export class App extends Component {
-  render() {
-    return (
-      <IndoorNav />
-    );
-  }
+export default function App() {
+ return(
+    <IndoorNav/>
+ );
 }
 
 
